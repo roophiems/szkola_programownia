478e984a21849217c1fc331592727fe5c478dfcd
Rafał <rrybak@future-processing.com>
2019-08-25 14:54:15 +0200

    :boom: Initial Commit

---
+++ src/pl/coderslab/utils/PasswordUtil.java
@@ -0,0 +1,13 @@
+package pl.coderslab.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public final class PasswordUtil {
+
+	private PasswordUtil() {}
+
+	public static String createHash(String password) {
+		return BCrypt.hashpw(password, BCrypt.gensalt());
+	}
+
+}
